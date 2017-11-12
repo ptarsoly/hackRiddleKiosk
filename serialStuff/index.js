b7ba52903f0c634256ec5d0f8de8c96c6776b543
ptarsoly <ptarsolyapps@ymail.com>
2017-11-12 07:06:05 -0500

    added serial comms

--- serialStuff/index.js
+++ serialStuff/index.js
@@ -1,20 +1,54 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var cors = require('cors');
+//var cors = require('cors');
 var app = express();
 
-app.use(cors());
+var SerialPort = require('serialport');
+var port = new SerialPort('/dev/tty96B0', { autoOpen: false });
+
+port.open(function (err) {
+    if (err) {
+      return console.log('Error opening port: ', err.message);
+    }
+});
+//app.use(cors());
 
 app.use(bodyParser.json());
 
+var jsonData = {
+    "user" : null
+};
+
 app.post('/', function(request, response){
   console.log(request.body);      // your JSON
-   response.send(request.body);    // echo the result back
+  jsonData.user = request.body.user;
+   response.send( ( verify() ) ? '1' : '0' );    // echo the result back
 });
+
 
 app.get('/data',function(request, response){
     response.send("Hello World");
 });
 
-app.listen(3001);
+app.listen(3001, "0.0.0.0");
 
+function verify() {
+    if(jsonData.user<4 && jsonData.user>-1) {
+        //serial code
+        var byteToSend = 0xc0;
+        byteToSend = byteToSend | jsonData.user;
+        port.write(byteToSend, function( error ) {
+            if(error) {
+                console.log(error);
+            }
+        });
+        var verified = port.read();
+        
+        if(verified.find('1')) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
